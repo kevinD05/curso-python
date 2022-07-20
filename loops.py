2cc886e8e9d9d0e900f1a9d69c56692393198414
Kevin Diaz <alexanderkevindiaz05@gmial.com>
2022-07-20 14:36:28 -0600

    loops2

--- loops.py
+++ loops.py
@@ -17,10 +17,30 @@
 #   print("hemos terminado")
  
 
-usuario =['chanchito feliz','felipe','roberto','nicolas']
+#usuario =['chanchito feliz','felipe','roberto','nicolas']
 
-edades = [24, 25, 26, 35]
+#edades = [24, 25, 26, 35]
 
-for usuario in usuario:
-    for edad in edades:
-        print(usuario, edades)
+#for usuario in usuario:
+   # for edad in edades:
+       #3 print(usuario, edades)
+
+def miFuncion():
+    print('mi primera funcion!')
+
+#miFuncion
+
+def imprimeDato(*nombre):
+    print("el nombre completo es:", nombre[1])
+
+#imprimeDato("chanchito",'feliz', 'lala',''lele)
+
+def nombreCompleto(apellido, nombre):
+    print(nombre, apellido)
+
+#nombreCompleto(nombre= 'chanchito', apellido = feliz)
+
+def nombrecompleto2(**kwargs):
+    print(kwargs['nombre'], kwargs['apellido'])
+
+nombrecompleto2(nombre= 'chanchito', apellido = 'feliz')
